19869b4ddcb50b94946e2f9f9cfc559578f12923
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-07 19:15:33 +0200

    commit

---
+++ src/shared/BattleCard/BattleCard.vue
@@ -0,0 +1,19 @@
+<template>
+     <div class="card">
+         <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  .card{
+    display: flex;
+    flex-direction: column;
+    width: 325px;
+    background-color: white;
+    padding: 15px;
+    border: 2px solid rgb(221, 91, 91);
+    box-shadow: 5px 5px 7px 1px #070707;;
+    border-radius: 25px;
+    overflow: hidden;
+  }
+</style>
