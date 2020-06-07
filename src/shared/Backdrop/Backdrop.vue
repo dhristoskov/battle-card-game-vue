a253b393e3219c1b89662db5e40c438bd47fe9f0
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-07 19:15:57 +0200

    commit

---
+++ src/shared/Backdrop/Backdrop.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="backdrop"></div>
+</template>
+
+<style lang="scss" scoped>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgba(201, 204, 206, 0.85);
+        z-index: 30;
+    }
+</style>
