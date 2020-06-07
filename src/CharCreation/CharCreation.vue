89956cb0c9ab47bcdb6b65b2f32f120374464106
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-07 19:13:52 +0200

    commit

---
+++ src/CharCreation/CharCreation.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="main-wrapper">
+        <h1 class="title">Choose Your Character</h1>
+        <div class="hero-list">
+          <hero-card v-for="hero in heroes"
+          :hero='hero' :key='hero.id'
+          :selectHero='selectHero'/>
+        </div>
+    </div>
+</template>
+
+<script>
+import HeroCard from './components/HeroCard'
+import { heroList } from '../shared/data'
+
+export default {
+  data () {
+    return {
+      heroes: heroList,
+      hero: ''
+    }
+  },
+  components: {
+    heroCard: HeroCard
+  },
+  methods: {
+    selectHero (hero) {
+      this.hero = hero
+      this.$router.push(`/battle/${this.hero}`)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .main-wrapper{
+    background-image: url('../assets/background.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 100vw;
+    min-height: 100vh;
+    z-index: -2;
+    .title {
+      -webkit-text-stroke: 2px black;
+      color: white;
+      font-size: 45px;
+      text-align: center;
+      padding: 45px 0;
+    }
+    .hero-list{
+      width: 60%;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      margin: auto;
+    }
+  }
+</style>
---
+++ src/CharCreation/components/HeroCard.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="hero-img" @click="selectHero(hero.name)">
+        <p>{{hero.name}}</p>
+        <img :src="getImageUrl(hero.img)" alt="hero">
+    </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'hero',
+    'selectHero'
+  ],
+  methods: {
+    getImageUrl (heroImg) {
+      return require('../../assets/' + heroImg)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .hero-img {
+        position: relative;
+        width:225px;
+        height: 325px;
+        border: 2px solid rgb(221, 91, 91);
+        box-shadow: 5px 5px 7px 1px #070707;;
+        border-radius: 25px;
+        overflow: hidden;
+        transition: all .3s ease-in;
+        background-color: white;
+        cursor: pointer;
+        &:hover {
+            border: 2px solid green;
+            transform: scale(1.1);
+        }
+        img {
+            padding: 10px;
+            width:225px;
+            height: 325px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 0;
+        }
+        p {
+            position: absolute;
+            top: 5px;
+            left: 35%;
+            color: black;
+            font-size: 20px;
+            font-weight: 700;
+            z-index: 1;
+        }
+    }
+</style>
