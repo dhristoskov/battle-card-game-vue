322204936efc9291ccc5bd94f4a8cbe7fe6883dd
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-07 19:14:59 +0200

    commit

---
+++ src/shared/Modal/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <backdrop/>
+        <div class="modal">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import Backdrop from '../Backdrop/Backdrop'
+
+export default {
+  components: {
+    backdrop: Backdrop
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .modal {
+        position: fixed;
+        z-index: 40;
+        background-color: white;
+        width: 30%;
+        border: 1px solid black;
+        box-shadow: 1px 1px 1px black;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        p {
+            position: fixed;
+            top: -25px;
+            right: -5px;
+            color: black;
+            text-align: center;
+            font-size: 12px;
+            font-weight: 700;
+            text-transform: uppercase;
+            padding: 2px 4px;
+            width: 50px;
+            margin-left: auto;
+            cursor: pointer;
+        }
+    }
+</style>
---
+++ src/shared/data.js
@@ -0,0 +1,62 @@
+export const monsterList = [
+  {
+    id: 1,
+    name: 'Fantasma',
+    minHit: 3,
+    maxHit: 10,
+    img: 'monster_3.jpg'
+  },
+  {
+    id: 2,
+    name: 'Baldazur',
+    minHit: 2,
+    maxHit: 13,
+    img: 'monster_6.jpg'
+  },
+  {
+    id: 3,
+    name: 'Kalira',
+    minHit: 6,
+    maxHit: 15,
+    img: 'monster_5.jpg'
+  },
+  {
+    id: 4,
+    name: 'Golze',
+    minHit: 6,
+    maxHit: 16,
+    img: 'monster_1.jpg'
+  },
+  {
+    id: 5,
+    name: 'Horon',
+    minHit: 2,
+    maxHit: 8,
+    img: 'monster_2.jpg'
+  },
+  {
+    id: 6,
+    name: 'Uaran',
+    minHit: 7,
+    maxHit: 15,
+    img: 'monster_4.jpg'
+  }
+]
+
+export const heroList = [
+  {
+    id: 1,
+    name: 'Warrior',
+    img: 'warrior.jpg'
+  },
+  {
+    id: 2,
+    name: 'Wizard',
+    img: 'magition.jpg'
+  },
+  {
+    id: 3,
+    name: 'Ranger',
+    img: 'archer.jpg'
+  }
+]
