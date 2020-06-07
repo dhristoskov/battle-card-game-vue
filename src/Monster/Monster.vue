5cfdfbd249c113101012274149a5d91241b32a23
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-07 19:14:24 +0200

    commit

---
+++ src/Monster/Monster.vue
@@ -0,0 +1,44 @@
+<template>
+    <battle-card>
+        <h3 class="name-tag">{{monster.name}}</h3>
+        <health-bar :health='monsterHealth' />
+        <attack-bar :monsterAtkCounter='monsterAtkCounter'/>
+        <img :src="getImageUrl(monster.img)" alt="monster">
+    </battle-card>
+</template>
+
+<script>
+import HealthBar from '../shared/HealthBar/HealthBar'
+import MonsterAttackBar from './components/MonsterAttackBar'
+import BattleCard from '../shared/BattleCard/BattleCard'
+
+export default {
+  props: [
+    'monsterHealth',
+    'monster',
+    'monsterAtkCounter'
+  ],
+  components: {
+    healthBar: HealthBar,
+    battleCard: BattleCard,
+    attackBar: MonsterAttackBar
+  },
+  methods: {
+    getImageUrl (monsterImg) {
+      return require('../assets/' + monsterImg)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .name-tag {
+      text-align: center;
+    }
+    img {
+      padding: 10px;
+      width: 300px;
+      height: 350px;
+      margin: auto;
+    }
+</style>
---
+++ src/Monster/components/MonsterAttackBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class='attack-bar'>
+        <p class="info-title">Attack Points</p>
+        <div class='atackbar' :style="{width: ( monsterAtkCounter * 10 ) +'%'}" >
+            <p>{{ monsterAtkCounter }}/10</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'monsterAtkCounter'
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+    .attack-bar{
+        position: relative;
+        margin-top: 5px;
+        border: 1px solid rgb(0, 0, 0);
+        border-radius: 25px;
+        .info-title {
+          position: absolute;
+          top: 1px;
+          left: 9px;
+          color: black;
+          font-size: 12px;
+          font-weight: 700;
+        }
+        .atackbar {
+          border-radius: 25px;
+          background: rgb(70, 134, 218);
+          height: 15px;
+          padding: 2px 0;
+          transition: width 700ms;
+          p {
+            position: absolute;
+            top: 0;
+            left: 47%;
+            font-size: 14px;
+          }
+        }
+    }
+</style>
